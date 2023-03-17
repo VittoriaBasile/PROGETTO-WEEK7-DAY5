abffbf3d50e97ff5d3ea4c6f8d4b6c29fdbf4704
VittoriaBasile <vichy.basile@gmail.com>
2023-03-17 16:53:04 +0100

    commit

--- details.js
+++ details.js
@@ -1,1 +1,35 @@
 const endpoint = "https://striveschool-api.herokuapp.com/api/product/";
+const token =
+  " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MGI0MmY4MWI0MjAwMTM5YjI3YjciLCJpYXQiOjE2NzkwMzYzNzAsImV4cCI6MTY4MDI0NTk3MH0.Bx73lB6yVNt7AUJEjfLxcitd5wgiPHhLCqZLryFCN5M";
+fetch(url, {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+  .then((resp) => resp.json())
+
+  .then((data) => {
+    const prodotti = data;
+    const row = document.getElementById("row");
+    row.innerHTML = "";
+    for (const prodotto of prodotti) {
+      const col = document.createElement("div");
+      col.className = "col";
+      col.innerHTML = ` <div class="card" style="width: 18rem">
+                                <img
+                                    style="object-fit: cover"
+                                    src= ${img.src.medium} 
+                                    class="card-img-top img-fluid"
+                                    alt="card-pic"
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">${prodotto.name}</h5>
+                                    <p class="card-text">${prodotto.brend}</p>
+                                    <p class="card-text">${prodotto.price}</p>
+                                    <p class="card-text" >${prodotto.description}</p>
+                                </div>
+                        </div>`;
+
+      row.appendChild(col);
+    }
+  });
